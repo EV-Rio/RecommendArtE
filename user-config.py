9c700652b97d441a2b90508f3ed52c04326daa52
EV-Rio <ev.rio@alumnos.upm.es>
2026-06-08 20:40:32 +0200

    Upload files

---
+++ user-config.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+family = 'commons' # Commons refers to Wikimedia Commons, the central repo for images and media
+mylang = 'commons' # Multilingual global project
+usernames['commons']['commons'] = 'TFGImageRecommenderProject'
+user_agent_description = 'ImageDownloaderTool (contact: wiwiw5701@gmail.com)'
